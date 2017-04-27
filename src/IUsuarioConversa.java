f1bab4eeb81c847f00be0754ed0772be480821b7
João Vitor <joao.vitor.oliveira963@gmail.com>
2017-04-26 23:10:08 -0300

    Alteração na assinatura do método leMensagens.

--- src/IUsuarioConversa.java
+++ src/IUsuarioConversa.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public interface IUsuarioConversa {
 
 	Usuario fazLogin(String nickname, String senha);
-	void leMensagens() throws IOException;
+	void leMensagens(String outroUsuario) throws IOException;
 	void escreveMensagem(String destinatario, String mensagem) throws IOException;
 	
 }
