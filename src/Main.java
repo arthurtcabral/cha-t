2d6581d857e284623fcdc6466602867240d1d142
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2017-04-27 21:12:36 -0300

    Adicionando o menu

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,64 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		teclado = new Scanner(System.in);
 		
-		String nomeArquivo = "";
+		String nomeArquivo = "Usuarios.txt";
 		UsuarioConversa usuarioConversa = new UsuarioConversa(new File(nomeArquivo));
+		int op = 0;
+		
+		do{
+			System.out.println("1 - logar");
+			System.out.println("2 - sair");
+			op = teclado.nextInt();
+			teclado.nextLine();
+		
+		if(op == 1){
+			System.out.println("Digite o usuario: ");
+			String usuario = teclado.next();
+			System.out.println("Digite a senha: ");
+			String senha = teclado.next();
 			
-		// TODO: Implementar menu de login.
-		// TODO: Implementar menu (1- Ler mensagens; 2- Escrever mensagem).
-		// TODO: Implementar Submenus (Para as escolhas 1 e 2).
+			Usuario user = usuarioConversa.fazLogin(usuario, senha);
+			if (user != null){
+				int opcao = 0;
+				do{
+				System.out.println("1 - ler mensagem");				
+				System.out.println("2 - escrever mensagem");
+				System.out.println("3 - fazer loggof");
+				opcao = teclado.nextInt();
+				teclado.nextLine();
+				
+				if(opcao == 1){
+					System.out.println("Digite o usuário da mensagem");
+					String outroUsuario = teclado.next();
+					teclado.nextLine();
+					usuarioConversa.leMensagens(outroUsuario);
+				}else if(opcao == 2){
+					System.out.println("Digite o destinatario");
+					String destinatario = teclado.next();
+					teclado.nextLine();
+					System.out.println("Digite a mensagem:");
+					String mensagem = teclado.nextLine();
+					usuarioConversa.escreveMensagem(destinatario, mensagem);
+				}else if(opcao == 3){
+					System.out.println("Fazendo loggof...");
+				}else{
+					System.out.println("Opcão inválida");
+				}
+				
+				}while(opcao != 3);
+				
+				
+			}else{
+				System.out.println("Usuário ou senha inválidos");				
+			}
+			
+		}else if(op == 2){
+			System.out.println("Encerrando programa...");
+		}else{
+			System.out.println("Opção inválida");
+		}
+		
+		}while(op != 2);
 
 	}
 
